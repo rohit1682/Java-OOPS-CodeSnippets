ff33ce18257e058b7e063fcaf110f734cbb6e087
Rohit Ghosh <rht.ghs10@gmail.com>
2025-11-05 17:55:16 +0530

    Doubt on Inheritance Solved

---
+++ Inheritance/Doubt/ConstructorExample.java
@@ -0,0 +1,41 @@
+class GrandParent {
+    GrandParent() {
+        System.out.println("GrandParent Constructor");
+    }
+    GrandParent(int x) {
+        System.out.println("GrandParent Constructor with value: " + x);
+    }
+}   
+
+class Parent {
+
+    /*
+     * If the parent class does not have a default constructor (a constructor with no parameters), and 
+     * the child calls the parent constructor using super() without arguments, it will not throw an error.
+     * the compiler created an empty default constructor for the parent class. 
+     */
+
+    Parent() {
+        System.out.println("Parent Constructor");
+    }
+    Parent(int x) {
+        // super();
+        System.out.println("Parent Constructor with value: " + x);
+    }
+}
+
+class Child extends Parent {
+    Child() {
+        super(); // calls Parent's constructor
+        System.out.println("Child Constructor");
+    }
+    Child(int x) {
+        super(x); // calls Parent's constructor with parameter
+        System.out.println("Child Constructor with value: " + x);
+    }
+}
+
+
+public class ConstructorExample {
+    
+}
